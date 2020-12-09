fb3d3c76f47141d32e971b0918e17d5e0dabe013
Kate Kim <gxk180021@utdallas.edu>
2020-12-09 15:37:43 -0600

    add functions to wach buttons and math function for equal button.

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,131 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView workingsTV;
+    TextView resultsTV;
+
+    String workings= "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initTextViews();
     }
+
+    private void initTextViews() {
+        workingsTV = (TextView)findViewById(R.id.workingsTextView);
+        resultsTV = (TextView)findViewById(R.id.resultTextView);
+    }
+
+    private void setWorkings(String givenValue){
+        workings = workings+givenValue;
+        workingsTV.setText(workings);
+
+    }
+
+    public void equalOnClick(View view) {
+        Double result = null;
+        ScriptEngine engine = new ScriptEngineManager(.getEngineByName("rhino"));
+
+        try{
+            result = (double)engine.eval(workings);
+
+        }catch (ScriptException e){
+            Toast.makeText(this, "Invalid Input", Toast.LENGTH_SHORT).show();
+        }
+        if (result != null) {
+
+            resultsTV.setText(String.valueOf(result.doubleValue()));
+        }
+    }
+
+
+    public void clearOnClick(View view) {
+        workingsTV.setText("");
+        workings = "";
+        resultsTV.setText("");
+    }
+
+    public void bracketsOnClick(View view) {
+
+    }
+
+    public void exponentOnClick(View view) {
+        setWorkings("^");
+    }
+
+    public void divideOnClick(View view) {
+        setWorkings("/");
+    }
+
+    public void sevenOnClick(View view) {
+        setWorkings("7");
+    }
+
+    public void eightOnClick(View view) {
+        setWorkings("8");
+    }
+
+    public void nineOnClick(View view) {
+        setWorkings("9");
+    }
+
+    public void multiplyOnClick(View view) {
+        setWorkings("x");
+    }
+
+    public void fourOnClick(View view) {
+        setWorkings("4");
+    }
+
+    public void fiveOnClick(View view) {
+        setWorkings("5");
+    }
+
+    public void sixOnClick(View view) {
+        setWorkings("6");
+    }
+
+    public void subtractOnClick(View view) {
+        setWorkings("-");
+    }
+
+    public void oneOnClick(View view) {
+        setWorkings("1");
+    }
+
+    public void twoOnClick(View view) {
+        setWorkings("2");
+    }
+
+    public void threeOnClick(View view) {
+        setWorkings("3");
+    }
+
+    public void addOnClick(View view) {
+        setWorkings("+");
+    }
+
+    public void pointOnClick(View view) {
+        setWorkings(".");
+    }
+
+    public void zeroOnClick(View view) {
+        setWorkings("0");
+    }
+
+
 }
+
